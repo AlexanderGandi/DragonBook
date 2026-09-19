ca9f3c4b6492a5d52f8ed0330b9c319f64f54969
AlexanderGandi <g4453842@gmail.com>
2026-09-19 13:05:51 +0800

    Parser definition

---
+++ Parser.h
@@ -0,0 +1,38 @@
+//
+// Created by ocean on 19/9/2026.
+//
+
+#ifndef DRAGONBOOK_PARSER_H
+#define DRAGONBOOK_PARSER_H
+#include "ASTNode.h"
+#include "Lexer.h"
+
+
+class Parser {
+private:
+    Lexer *lexer_;
+
+    ASTNode *expr();
+    ASTNode *assignment();
+    ASTNode *conditional();
+    ASTNode *logicalOr();
+    ASTNode *logicalAnd();
+    ASTNode *exclusiveOrExpression();
+    ASTNode * andExpression();
+    ASTNode * equalityExpression();
+    ASTNode * relationalExpression();
+    ASTNode * shiftExpression();
+    ASTNode * additiveExpression();
+    ASTNode * multiplicativeExpression();
+    ASTNode * unaryExpression();
+    ASTNode * unaryExpressionNotPlusMinus();
+    ASTNode * postfixExpression();
+    ASTNode * primary();
+    ASTNode *assignmentOperator();
+
+public:
+    Parser(Lexer *lexer) : lexer_(lexer) {}
+};
+
+
+#endif //DRAGONBOOK_PARSER_H
